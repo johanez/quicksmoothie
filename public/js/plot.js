319eb1c1a57bb80b5e4f27e4412c98b6ab85fac8
johanez <johannes.eberenz@gmai.com>
2015-05-17 19:15:18 +0200

    added y and z time series to chart

--- public/js/plot.js
+++ public/js/plot.js
@@ -2,8 +2,9 @@
 // Randomly add a data point every 500ms
 var tsx = new TimeSeries();
 var tsy = new TimeSeries();
+var tsz = new TimeSeries();
 //
-gyro.frequency = 500;
+gyro.frequency = 100;
 gyro.startTracking(function(o) {
 	var f = document.getElementById('gyrofeatures');
 	var b = document.getElementById('gyroreadout');
@@ -13,6 +14,7 @@ gyro.startTracking(function(o) {
 		t = new Date().getTime();
 		tsx.append(t, o.x);
 		tsy.append(t, o.y);
+		tsz.append(t, o.z - 9.81);
 		b.innerHTML = 	"<p> x = " + o.x + "</p>" +
 						"<p> y = " + o.y + "</p>" +
 						"<p> z = " + o.z + "</p>" +
@@ -34,12 +36,15 @@ gyro.startTracking(function(o) {
 // }, 500);
 
  function createTimeline() {
-   var chart = new SmoothieChart();
+   var chart = new SmoothieChart({millisPerPixel:50});
    chart.addTimeSeries(tsx, { 	strokeStyle: 'rgba(0, 255, 0, 1)', 
    							 	fillStyle: 'rgba(0, 255, 0, 0.2)', 
    							 	lineWidth: 2 });
-   chart.addTimeSeries(tsy, { strokeStyle: 'rgba(255, 0, 0, 1)', 
-   							  fillStyle: 'rgba(255, 0, 0, 0.2)', 
-   							  lineWidth: 2 });
-   chart.streamTo(document.getElementById("chart"), 1000);
+   chart.addTimeSeries(tsy, { 	strokeStyle: 'rgba(255, 0, 0, 1)', 
+   							  	fillStyle: 'rgba(255, 0, 0, 0.2)', 
+   							  	lineWidth: 2 });   
+   chart.addTimeSeries(tsz, { 	strokeStyle: 'rgba(0, 0, 255, 1)', 
+   							  	fillStyle: 'rgba(0, 0, 255, 0.2)', 
+   							  	lineWidth: 2 });
+   chart.streamTo(document.getElementById("chart"), 500);
  }
